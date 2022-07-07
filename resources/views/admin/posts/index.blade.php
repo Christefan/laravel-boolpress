fc6a566704ec945329c6a6bd59be8c41dc56ffe6
Christefan <chriscapod7@gmail.com>
2022-07-07 02:08:52 +0200

    Modifica pagine admin/post

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -2,20 +2,16 @@
 
 @section('content')
     <h1>Lista di tuoi post</h1>
-
-    <div class="row row-cols-3">
+    <div class="row row-cols-4">
         @foreach ($posts as $post)
-            {{-- Single post --}}
             <div class="col">
-                <div class="card mb-3" style="width: 18rem;">
-                    {{-- <img class="card-img-top" src="..." alt="Card image cap"> --}}
+                <div class="card mb-5 py-2 border-dark" style="width: 23rem; min-height: 10rem;">
                     <div class="card-body">
                         <h5 class="card-title">{{ $post->title }}</h5>
                         <a href="{{ route('admin.posts.show', ['post' => $post->id]) }}" class="btn btn-primary">Leggi post</a>
                     </div>
                 </div>
             </div>
-            {{-- /Single post --}}
         @endforeach
     </div>
 @endsection
--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -1,14 +1,15 @@
 @extends('layouts.dashboard')
 
 @section('content')
-    <h1> {{ $post->title }} </h1>
-    <p>Slug: {{ $post->slug }}</p>
-    <p>{{ $post->content }}</p>
-
+    <h1>{{$post->title}}</h1>
+    <h2>Slug:{{$post->slug}}</h2>
+    <p>{{$post->content}}</p>
     <a class="btn btn-primary" href="{{ route('admin.posts.edit', ['post' => $post->id]) }}">Modifica</a>
+    
+    
     <form action="{{ route('admin.posts.destroy', [ 'post' => $post->id ]) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger">Cancella</button>
+        @csrf
+        @method('DELETE')
+        <button type="submit" class="btn btn-danger">Cancella</button>
     </form>
 @endsection
